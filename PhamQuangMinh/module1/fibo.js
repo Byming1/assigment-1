d4c3ad864631861db574443ed585d6a0567f875e
Byming1 <pqm1290@gmail.com>
2026-01-22 11:18:46 +0700

    feat: Implement iterative Fibonacci (flag=false)

--- PhamQuangMinh/module1/fibo.js
+++ PhamQuangMinh/module1/fibo.js
@@ -1,5 +1,15 @@
 function Fibonacci(n, flag) {
-    throw new Error("Not implemented");
+    if (flag === false) {
+        if (n <= 1) return n;
+        let a = 0, b = 1;
+        for (let i = 2; i <= n; i++) {
+            let temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+    throw new Error("flag must be false for iterative implementation");
 }
 
 module.exports = Fibonacci;
